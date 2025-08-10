9dd3b605c656514a8577a85d8ae93f0ef49905f3
otakakot <kotaro.otaka@gmail.com>
2025-08-10 16:46:17 +0900

    first commit

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.6
 require (
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.5
+	github.com/testcontainers/testcontainers-go v0.38.0
 	github.com/testcontainers/testcontainers-go/modules/compose v0.38.0
 )
 
@@ -154,7 +155,6 @@ require (
 	github.com/spf13/cobra v1.9.1 // indirect
 	github.com/spf13/pflag v1.0.6 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
-	github.com/testcontainers/testcontainers-go v0.38.0 // indirect
 	github.com/theupdateframework/notary v0.7.0 // indirect
 	github.com/tilt-dev/fsnotify v1.4.8-0.20220602155310-fff9c274a375 // indirect
 	github.com/tklauser/go-sysconf v0.3.12 // indirect
--- test/internal/testx/testx.go
+++ test/internal/testx/testx.go
@@ -2,10 +2,103 @@ package testx
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
+	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/compose"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
+
+func SetupContainer(
+	t *testing.T,
+) string {
+	t.Helper()
+
+	user := "test"
+	password := "test"
+	db := "test"
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	schemaPath := filepath.Join(filepath.Dir(pwd), "schema")
+
+	if _, err := os.Stat(schemaPath); os.IsNotExist(err) {
+		t.Fatalf("Schema directory does not exist: %s", schemaPath)
+	}
+
+	sqlFiles, err := filepath.Glob(filepath.Join(schemaPath, "*.sql"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(sqlFiles) == 0 {
+		t.Fatalf("No SQL files found in schema directory: %s", schemaPath)
+	}
+
+	containerFiles := make([]testcontainers.ContainerFile, 0, len(sqlFiles))
+
+	for _, sqlFile := range sqlFiles {
+		fileName := filepath.Base(sqlFile)
+		containerFiles = append(containerFiles, testcontainers.ContainerFile{
+			HostFilePath:      sqlFile,
+			ContainerFilePath: "/docker-entrypoint-initdb.d/" + fileName,
+			FileMode:          0644,
+		})
+	}
+
+	container, err := testcontainers.GenericContainer(
+		t.Context(),
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: testcontainers.ContainerRequest{
+				Image:        "postgres:17-alpine",
+				ExposedPorts: []string{"5432/tcp"},
+				Env: map[string]string{
+					"TZ":                        "UTC",
+					"LANG":                      "ja_JP.UTF-8",
+					"POSTGRES_DB":               db,
+					"POSTGRES_USER":             user,
+					"POSTGRES_PASSWORD":         password,
+					"POSTGRES_INITDB_ARGS":      "--encoding=UTF-8",
+					"POSTGRES_HOST_AUTH_METHOD": "trust",
+				},
+				Cmd: []string{"postgres", "-c", "log_statement=all"},
+				WaitingFor: wait.ForAll(
+					wait.ForListeningPort("5432/tcp"),
+					wait.ForExec([]string{"pg_isready", "-U", user, "-d", db}).
+						WithPollInterval(1*time.Second).
+						WithExitCodeMatcher(func(exitCode int) bool {
+							return exitCode == 0
+						}).
+						WithStartupTimeout(30*time.Second),
+				),
+				Files: containerFiles,
+			},
+			Started: true,
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	testcontainers.CleanupContainer(t, container)
+
+	host, err := container.Host(t.Context())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	port, err := container.MappedPort(t.Context(), "5432/tcp")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return "postgres://" + user + ":" + password + "@" + host + ":" + port.Port() + "/" + db + "?sslmode=disable"
+}
 
 func SetupCompose(
 	t *testing.T,
